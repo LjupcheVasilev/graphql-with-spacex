93dc5d4e45d4167ab13048a948b8fc81ad29002f
Ljupche Vasilev <ljupche.vasilev@gmail.com>
2023-12-17 16:28:38 +0000

    SpaceX: Launchapd and City resolvers

--- src/Application/resolvers/index.ts
+++ src/Application/resolvers/index.ts
@@ -1,4 +1,18 @@
 export const resolvers = {
+  Launchpad: {
+    city: async (parent: any, args: any, context: any, info: any) => {
+      return await fetch(`https://api.geodatasource.com/city?key=50QWVCVPE7ID8PGZNTUPYTKBPANQ5BTH&format=json&lat=${parent.latitude}&lng=${parent.longitude}`).then((res) => res.json());
+    }
+  },
   Query: {
+    launchpads: async (parent: any, args: any, context: any, info: any) => {
+      return await fetch('https://api.spacexdata.com/v4/launchpads').then((res) => res.json());
+    },
+    launchpad: async (parent: any, args: any, context: any, info: any) => {
+      return await fetch(`https://api.spacexdata.com/v4/launchpads/${args.id}`).then((res) => res.json());
+    },
+    city: async (parent: any, args: any, context: any, info: any) => {
+      return await fetch(`https://api.geodatasource.com/city?key=50QWVCVPE7ID8PGZNTUPYTKBPANQ5BTH&format=json&lat=${args.lat}&lng=${args.lon}`).then((res) => res.json());
+    }
   },
 };
--- src/Domain/schema.ts
+++ src/Domain/schema.ts
@@ -1,5 +1,25 @@
 export const typeDefs = `#graphql
 
+  type Launchpad {
+    id: ID!
+    name: String!
+    latitude: Float!
+    longitude: Float!
+    details: String
+    city: City!
+  }
+
+  type City {
+    city: String!
+    region: String!
+    sunrise: String!
+    sunset: String!
+    country: String!
+  }
+
   type Query {
+    launchpads: [Launchpad!]!
+    launchpad(id: ID!): Launchpad!
+    city(lat: Float!, lon: Float!): City!
   }
 `;
